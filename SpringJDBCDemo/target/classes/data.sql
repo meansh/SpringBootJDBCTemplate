974fe70b3ef1954cf13c0a60782e289b1104d414
Ansh Srivastava <42718094+meansh@users.noreply.github.com>
2024-01-28 22:45:16 +0530

    Spring JDBC Template
    When we want to interact with DB using JDBC, we want to connect using the piece of code like, Connection con = new Connection..etc, which was lengthy. but with the help of JDBCTemplate, we can all let it go.
    It's a shortcut using the spring boot features.

---
+++ SpringJDBCDemo/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id, name, tech) values (101, 'Ansh', 'Blockchain');
+insert into alien (id, name, tech) values (102, 'Aman', 'Blockchain');
+insert into alien (id, name, tech) values (103, 'Aaditya', 'Blockchain');
